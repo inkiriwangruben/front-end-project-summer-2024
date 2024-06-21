68d643329756d80495a2e27e375451f90a7b3485
ruben <inkiriwangruben@gmail.com>
2024-06-21 08:33:38 +0700

    Menambahkan fitur baru

--- pertemuan_14/index.js
+++ pertemuan_14/index.js
@@ -3,4 +3,8 @@ console.log("latihan git")
 =======
 console.log("latihan git")
 console.log("latihaannnnn")
+<<<<<<< HEAD
 >>>>>>> 1c01ee4d17be95a262f997f16383aef57d52a391
+=======
+console.log("mengatasi git")
+>>>>>>> afca0d9be2ebb490db2e5ec5ad4ff7ece01bd0b3
